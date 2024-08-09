8af12543dc7561e1ebea262cb4ddcc635a2f1d8d
Bayode <laoye.bayode@gmail.com>
2024-08-09 11:14:17 -0400

    Worked on route

--- app/api/porfolio_routes.py
+++ app/api/porfolio_routes.py
@@ -7,7 +7,5 @@ portfolio_routes = Blueprint("portfolio", __name__)
 @portfolio_routes.route('/<int:user_id>')
 # @login_required
 def user_portfolio(user_id):
-    portfolios = Portfolio.query.filter(user_id="user_id").all()
-    # users = User.query.filter(User.name = 'Will').all()
-    # portfolios = Portfolio.query.get(user_id="user_id").all()
-    return {"Portfolios": [portfolios.to_dict() for portfolio in portfolios]}
+    portfolios = Portfolio.query.filter(Portfolio.user_id==user_id).all()
+    return {"Portfolios": [portfolio.to_dict() for portfolio in portfolios]}
