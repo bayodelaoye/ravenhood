9189b1c60d44428099979d1d20beb180b017100a
Scott Feichter <scottfeichter@gmail.com>
2024-08-09 16:23:52 -0700

    misc (msg via sh alias)

---
+++ react-vite/src/components/Stocks/scratchFrontEndPortfolio.js
@@ -0,0 +1,29 @@
+
+
+
+fontEndPortfolio{
+
+  portfolios.id.1.portfolio_name: TylerRetirement,
+  portfolios.id.1.cash_balance: 1000000,
+
+  portfolios_stocks.portfolio_id: 1,
+  stocks: {
+    portfolios_stocks.stock_id: 3,
+    portfolios_stocks.stock_id: 7,
+
+}
+
+
+
+useful frontend functions
+
+getPortfolioStockTodaysPrice(ticker){}
+getPortoflioStockAllBuys(ticker, portfolio_id){}
+getPortfolioStockAllSells(ticker, portfolio_id){}
+getPortfolioStockAllTransactions(ticker, portfolio_id){}
+getPortfolioStockQuantity(ticker, portfolio_id){}
+calculatePortfolioStockValue(getPortfolioStockQuantity, getPortfolioStockTodaysPrice){}
+
+
+getPortfolioName(portfolio_id){}
+getPortfolioCashBalance(portfolio_id){}
