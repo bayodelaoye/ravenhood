c52ca5a43f260d892e0d5dd65eb4748bcdae6410
Tayon <tayondw@gmail.com>
2024-08-12 12:43:35 -0400

    loaders lecture to go over, make sure to clean up sign up and login form component and route provider

--- react-vite/src/components/SignupFormPage/SignupFormPage.jsx
+++ react-vite/src/components/SignupFormPage/SignupFormPage.jsx
@@ -2,93 +2,99 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
 import { thunkSignup } from "../../redux/session";
+import { useLoaderData } from "react-router-dom";
 
 function SignupFormPage() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const sessionUser = useSelector((state) => state.session.user);
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [errors, setErrors] = useState({});
+	const dispatch = useDispatch();
+      const stocks = useLoaderData();
+      const portfolios = useLoaderData();
+	const navigate = useNavigate();
+	const sessionUser = useSelector((state) => state.session.user);
+	const [email, setEmail] = useState("");
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+      const [errors, setErrors] = useState({});
+      
+      console.log("stocks", stocks);
+      console.log("portfolios", portfolios);
 
-  if (sessionUser) return <Navigate to="/" replace={true} />;
+	if (sessionUser) return <Navigate to="/" replace={true} />;
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    if (password !== confirmPassword) {
-      return setErrors({
-        confirmPassword:
-          "Confirm Password field must be the same as the Password field",
-      });
-    }
+		if (password !== confirmPassword) {
+			return setErrors({
+				confirmPassword:
+					"Confirm Password field must be the same as the Password field",
+			});
+		}
 
-    const serverResponse = await dispatch(
-      thunkSignup({
-        email,
-        username,
-        password,
-      })
-    );
+		const serverResponse = await dispatch(
+			thunkSignup({
+				email,
+				username,
+				password,
+			}),
+		);
 
-    if (serverResponse) {
-      setErrors(serverResponse);
-    } else {
-      navigate("/");
-    }
-  };
+		if (serverResponse) {
+			setErrors(serverResponse);
+		} else {
+			navigate("/");
+		}
+	};
 
-  return (
-    <>
-      <h1>Sign Up</h1>
-      {errors.server && <p>{errors.server}</p>}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        {errors.username && <p>{errors.username}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button type="submit">Sign Up</button>
-      </form>
-    </>
-  );
+	return (
+		<>
+			<h1>Sign Up</h1>
+			{errors.server && <p>{errors.server}</p>}
+			<form onSubmit={handleSubmit}>
+				<label>
+					Email
+					<input
+						type="text"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.email && <p>{errors.email}</p>}
+				<label>
+					Username
+					<input
+						type="text"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.username && <p>{errors.username}</p>}
+				<label>
+					Password
+					<input
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.password && <p>{errors.password}</p>}
+				<label>
+					Confirm Password
+					<input
+						type="password"
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+				<button type="submit">Sign Up</button>
+			</form>
+		</>
+	);
 }
 
 export default SignupFormPage;
--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,24 +1,27 @@
-import { createBrowserRouter } from 'react-router-dom';
-import LoginFormPage from '../components/LoginFormPage';
-import SignupFormPage from '../components/SignupFormPage';
-import Layout from './Layout';
+import { createBrowserRouter } from "react-router-dom";
+import LoginFormPage from "../components/LoginFormPage";
+import SignupFormPage from "../components/SignupFormPage";
+import Layout from "./Layout";
+import { stocksLoader, allLoader } from "../loaders";
 
 export const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <h1>Welcome!</h1>,
-      },
-      {
-        path: "login",
-        element: <LoginFormPage />,
-      },
-      {
-        path: "signup",
-        element: <SignupFormPage />,
-      },
-    ],
-  },
+	{
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <h1>Welcome!</h1>,
+			},
+			{
+                        path: "login",
+                        loader: stocksLoader,
+				element: <LoginFormPage />,
+			},
+			{
+                        path: "signup",
+                        loader: allLoader,
+				element: <SignupFormPage />,
+			},
+		],
+	},
 ]);
