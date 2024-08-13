80d79c202b4feffc6dd7d772b6fd6e47745c8005
Scott Feichter <scottfeichter@gmail.com>
2024-08-13 16:39:09 -0700

    misc (msg via sh alias)

--- react-vite/src/components/LoginFormPage/LoginFormPage.jsx
+++ react-vite/src/components/LoginFormPage/LoginFormPage.jsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
 import { thunkLogin } from "../../redux/session";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useNavigate, useLoaderData } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import "./LoginForm.css";
 
 function LoginFormPage() {
   const navigate = useNavigate();
-  const allStocks = useLoaderData();
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
 
-  console.log("here is all stocks", allStocks);
 
   if (sessionUser) return <Navigate to="/" replace={true} />;
 
@@ -39,12 +37,6 @@ function LoginFormPage() {
       <h1>Log In</h1>
       {errors.length > 0 &&
         errors.map((message) => <p key={message}>{message}</p>)}
-      {allStocks.map((stock) => (
-        <div key={stock.id}>
-          {stock.company_name}
-          {stock.average_volume}
-        </div>
-      ))}
       <form onSubmit={handleSubmit}>
         <label>
           Email
