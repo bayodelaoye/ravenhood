61a3f988938f4f987af0af0758d93a43e1c13c28
Tayon <tayondw@gmail.com>
2024-08-13 19:34:07 -0400

    corrected comments and removed logs in portfolios.js in loaders folder

--- react-vite/src/components/loaders/portfolios.js
+++ react-vite/src/components/loaders/portfolios.js
@@ -6,9 +6,9 @@ const store = configureStore();
 export const userPortfolios = async ({ params }) => {
       await store.dispatch(thunkAuthenticate()); // making sure the user session state is up to date and not using the previous state
 
-	const state = store.getState(); // getting the previous state
+	const state = store.getState(); // getting the updated state
       const currentUser = state.session.user;
-      console.log(currentUser);
+      // console.log(currentUser);
       
 	const userId = params?.id || currentUser?.id;
 
@@ -24,7 +24,7 @@ export const userPortfolios = async ({ params }) => {
 		response.headers.get("content-type")?.includes("application/json")
 	) {
 		const userPortfolios = await response.json();
-		console.log("HERE", userPortfolios);
+		// console.log("HERE", userPortfolios);
 
 		return userPortfolios.portfolios;
 	} else {
