840e472ec38ab0c77b562c98650cd1b05742edd0
Scott Feichter <scottfeichter@gmail.com>
2024-08-14 08:54:34 -0700

    merging

--- react-vite/src/components/NavigationStart/Offerings/Invest.jsx
+++ react-vite/src/components/NavigationStart/Offerings/Invest.jsx
@@ -7,7 +7,8 @@ function Invest() {
     return(
         <>
             <main id="InvestMain">
-                <h1 id="InvestH1">Coming Soon</h1>
+                <h1 id="InvestH1">Coming Soon...</h1>
+                <h2 id="InvestH2">Invest Offerings</h2>
             </main>
 
 
