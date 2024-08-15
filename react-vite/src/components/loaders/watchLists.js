350383e3111567f4dc2bc5fc4bf37c7f83ade122
tyler <tylothedino@gmail.com>
2024-08-15 09:44:19 -0700

    Merged watchlist with testingbranch

--- react-vite/src/components/loaders/watchLists.js
+++ react-vite/src/components/loaders/watchLists.js
@@ -2,6 +2,7 @@ export const watchlistLoader = async () => {
     const response = await fetch(`/api/watch_lists/`)
     if (response.ok) {
         const userWatchlists = await response.json();
+        // Fixed this
         return { userWatchlists };
     }
     return null
