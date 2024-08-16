c7d113d08c0ea1c35c419f1bf6340962fce7a6d1
Scott Feichter <scottfeichter@gmail.com>
2024-08-15 23:01:07 -0700

    adjsuted searchbar maginifying glass size

--- react-vite/src/components/Navigation/SearchResults/SearchResults.jsx
+++ react-vite/src/components/Navigation/SearchResults/SearchResults.jsx
@@ -1,22 +1,22 @@
 import { useSelector } from "react-redux";
-import Spot from "../../Spot";
+import Stock from "../../Stock";
 import './SearchResults.css';
 
 
 const SearchResults = () => {
 
     const searchResults = useSelector(state => state.spots.allSpots);
-    // console.log("searchReasults=========", searchResults)
+    console.log("searchReasults=========", searchResults)
 
 
-    // console.log("SEARCH RESULTS COMPONENT RAN");
+    console.log("SEARCH RESULTS COMPONENT RAN");
     return (
     <>
-    <h1 id="searchResultsH1">All Spots</h1>
+    <h1 id="searchResultsH1">All Results</h1>
     <div id="searchResults">
 
         {searchResults ?
-        searchResults.map(result => <Spot key={result.id} spot={result} />) :
+        searchResults.map(result => <Stock key={result.id} spot={result} />) :
         <p>sorry, no results</p> }
 
     </div>
