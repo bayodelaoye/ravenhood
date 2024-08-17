297424c5d78bb958b6ec409095185b46683a7b81
Bayode <laoye.bayode@gmail.com>
2024-08-17 19:08:55 -0400

    Updated btn state

--- react-vite/src/components/Navigation/AccountDropdown.jsx
+++ react-vite/src/components/Navigation/AccountDropdown.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import platinum from "./Logo/platinum.png"
+import platinum from "./Logo/platinum.png";
 import { FaUserCircle } from "react-icons/fa";
 import { FaBriefcase } from "react-icons/fa6";
 import { BsBank2 } from "react-icons/bs";
@@ -16,41 +16,37 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import "./AccountDropdown.css";
 
-
-
 function AccountDropdown() {
+  const sessionUser = useSelector((store) => store.session.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const sessionUser = useSelector((store) => store.session.user);
-    const navigate = useNavigate()
-    const dispatch = useDispatch();
+  // ===================menu buttons handlers======================
 
+  const handleProfile = (e) => {
+    e.preventDefault();
+    navigate("/profile/portfolios");
+    console.log("handleProfile ran");
+  };
 
-    // ===================menu buttons handlers======================
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.thunkLogout());
+    console.log("handleLogout ran");
+    return navigate("/");
+  };
 
+  return (
+    <>
+      <ul id="AccountDropdownUl">
+        <li className="AccountDropdownUlLi">
+          <p
+            className="AccountDropdownName"
+            id="AccountDropdownUserName"
+          >{`${sessionUser.first_name} ${sessionUser.last_name}`}</p>
+        </li>
 
-    const handleProfile = (e) => {
-        e.preventDefault();
-        navigate("/profile/portfolios")
-        console.log("handleProfile ran");
-    }
-
-    const handleLogout = (e) => {
-        e.preventDefault();
-        dispatch(sessionActions.thunkLogout());
-        console.log("handleLogout ran");
-        return navigate('/')
-    }
-
-
-    return (
-        <>
-            <ul id="AccountDropdownUl">
-
-                <li className="AccountDropdownUlLi">
-                    <button className="AccountDropdownUlLiButton" id="AccountDropdownUserName">{`${sessionUser.first_name} ${sessionUser.last_name}`}</button>
-                </li>
-
-                {/* <li className="AccountDropdownUlLi">
+        {/* <li className="AccountDropdownUlLi">
                     <button className="AccountDropdownUlLiButton">
                         <img
                             id="platinum"
@@ -62,14 +58,14 @@ function AccountDropdown() {
                     </button>
                 </li> */}
 
-                <li className="AccountDropdownUlLi">
-                    <button
-                        onClick={handleProfile}
-                        className="AccountDropdownUlLiButton">
-                        <FaUserCircle className="AccountDropdownIcon" />Profile</button>
-                </li>
+        <li className="AccountDropdownUlLi">
+          <button onClick={handleProfile} className="AccountDropdownUlLiButton">
+            <FaUserCircle className="AccountDropdownIcon" />
+            Profile
+          </button>
+        </li>
 
-                {/* <li className="AccountDropdownUlLi">
+        {/* <li className="AccountDropdownUlLi">
                     <button className="AccountDropdownUlLiButton"><FaBriefcase className="AccountDropdownIcon" />Investing</button>
                 </li>
 
@@ -112,20 +108,19 @@ function AccountDropdown() {
                     <button className="AccountDropdownUlLiButton"><ImKeyboard className="AccountDropdownIcon" />Keyboard Shortcuts</button>
                 </li> */}
 
-
-                <li className="AccountDropdownUlLi">
-                    <button
-                        onClick={handleLogout}
-                        className="AccountDropdownUlLiButton"
-                        id="AccountDropdownLogOut">
-                        <MdLogout className="AccountDropdownIcon" />Log Out
-                    </button>
-                </li>
-
-
-            </ul>
-        </>
-    )
+        <li className="AccountDropdownUlLi">
+          <button
+            onClick={handleLogout}
+            className="AccountDropdownUlLiButton"
+            id="AccountDropdownLogOut"
+          >
+            <MdLogout className="AccountDropdownIcon" />
+            Log Out
+          </button>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default AccountDropdown;
