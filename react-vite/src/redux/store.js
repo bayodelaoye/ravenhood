55ee2f959b9f512b30fa86e5dff5f5b0361f388a
Scott Feichter <scottfeichter@gmail.com>
2024-08-18 14:50:37 -0700

    add to configureStore the conlog inside configureStore - store.getState():

--- react-vite/src/redux/store.js
+++ react-vite/src/redux/store.js
@@ -10,6 +10,7 @@ import portfolioReducer from "./portfolio";
 import watchlistReducer from "./watchlist";
 import navStocksReducer from "./navStocksRedux";
 import transactionReducer from "./transactions";
+import store from "redux"
 
 const rootReducer = combineReducers({
   session: sessionReducer,
@@ -30,6 +31,8 @@ if (import.meta.env.MODE === "production") {
 }
 
 const configureStore = (preloadedState) => {
+  console.log("inside configureStore - store.getState(): ", store.getState());
+  console.log("CONFIGURE STORE RAN - RETURNS :", "ROOTREDUCER", rootReducer, "PRELOADED STATE: ", preloadedState, "ENHANCER: ", enhancer)
   return createStore(rootReducer, preloadedState, enhancer);
 };
 
